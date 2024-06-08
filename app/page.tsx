e060934612d37aceb5cce2d99b2bfcb843ba4045
Chase Christensen <chaserchristensen@gmail.com>
2024-06-07 18:27:44 -0600

    new logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
-import logo from "../public/logomark.svg";
+import Logo from "./components/Logo";
 
 export default function Home() {
   const currentYear = new Date().getFullYear();
   return (
     <main className="flex min-h-[100dvh] flex-col items-start justify-between p-6 md:p-20">
       <div className="content flex flex-row gap-[3vw] items-center">
-        <div className="w-[50px] md:min-w-[100px] md:w-[10vw] ">
-          <Image src={logo} alt="Logo" />
+        <div className="w-[50px] md:min-w-[120px] md:w-[10vw] ">
+          <Logo />
         </div>
         <div className="flex-col flex">
-          <h1 className="text-[5vw] font-semibold">Industrial Web Company</h1>
+          <h1 className="text-[4.3vw] font-semibold">Industrial Web Company</h1>
         </div>
       </div>
       <div className="flex flex-row gap-5">
