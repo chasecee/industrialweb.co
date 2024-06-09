b949faaf04af7570bf47f0c79713f3034205fa38
Chase Christensen <chaserchristensen@gmail.com>
2024-06-09 14:27:25 -0600

    🐛 (page.tsx): Fix shadow generation loop to run only 6 iterations instead of 16 to optimize performance. Update CSS classes to remove unnecessary tracking property to resolve styling issues.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const currentYear = new Date().getFullYear();
 
   let shadow = "";
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 6; i++) {
     shadow += `${(shadow ? "," : "") + -i * 0.05}vw ${i * 0.05}vw 0 #000`;
   }
 
@@ -14,7 +14,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-[100dvh] flex-col items-start justify-between p-6 md:p-20 tracking-[-0.05em]">
+    <main className="flex min-h-[100dvh] flex-col items-start justify-between p-6 md:p-20 ">
       <div className="content flex flex-row gap-[3vw] items-center">
         <div className="w-[50px] md:min-w-[120px] md:w-[10vw] ">
           <Logo />
