5d70de927d6cb5e8eb19d3b9bb8e6abd842125ff
Chase Christensen <chaserchristensen@gmail.com>
2024-06-09 14:35:05 -0600

    🔧 (favicon): update favicon and various icon files   ✨ (safari-pinned-tab.svg): add new svg file for Safari pinned tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,31 @@ export const metadata: Metadata = {
     icon: "/icons/favicon.ico",
     shortcut: "/icons/favicon-32x32.png",
     apple: "/icons/apple-touch-icon.png",
+    other: [
+      {
+        rel: "icon",
+        url: "/icons/favicon-16x16.png",
+        type: "image/png",
+        sizes: "16x16",
+      },
+      {
+        rel: "icon",
+        url: "/icons/android-chrome-192x192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        rel: "icon",
+        url: "/icons/android-chrome-512x512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+      {
+        rel: "mask-icon",
+        url: "/icons/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
   },
 };
 
